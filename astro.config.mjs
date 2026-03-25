2e30e159a5f807005e0c9283e469ac1e0783e2aa
Marcos <63300985+emaildoissa@users.noreply.github.com>
2026-03-25 14:05:18 -0300

    feat: scaffold initial Astro website with main page content, Tailwind CSS, and SEO configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://www.antoniocarlosrosa.adv.br',
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [sitemap()]
+});
